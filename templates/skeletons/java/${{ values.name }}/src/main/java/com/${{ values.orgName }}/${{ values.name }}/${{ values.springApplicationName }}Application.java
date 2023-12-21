3fd0e13781c8da84f6d6b3518579c40ae2a56394
Rafael Sulimann <sulimannrafael@gmail.com>
2023-12-21 13:44:00 -0300

    Atualizando arquivos

--- templates/skeletons/java/${{ values.name }}/src/main/java/com/${{ values.orgName }}/${{ values.name }}/${{ values.springApplicationName }}Application.java
+++ templates/skeletons/java/${{ values.name }}/src/main/java/com/${{ values.orgName }}/${{ values.name }}/${{ values.springApplicationName }}Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DefaultSpringApplication {
+public class ${{ values.springApplicationName }}Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DefaultSpringApplication.class, args);
+		SpringApplication.run(${{ values.springApplicationName }}Application.class, args);
 	}
 
 }
