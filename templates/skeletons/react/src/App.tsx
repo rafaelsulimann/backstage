e258feb1462442dd540f09f906a4b928ac7639be
Rafael Sulimann <sulimannrafael@gmail.com>
2024-02-28 19:49:28 -0300

    feat: Melhorando o template do react

--- templates/skeletons/react/src/App.tsx
+++ templates/skeletons/react/src/App.tsx
@@ -1,5 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { TokenProvider } from "./contexts/context-token";
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <TokenProvider>
+      <Outlet />;
+    </TokenProvider>
+  );
 }
