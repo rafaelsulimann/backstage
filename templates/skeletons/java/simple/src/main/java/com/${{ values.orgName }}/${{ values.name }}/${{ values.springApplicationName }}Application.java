ddd87d4349e2679723461fe8d9d4371b98ec8057
Rafael Sulimann <sulimannrafael@gmail.com>
2024-03-28 08:59:06 -0300

    fix: Resolvendo problema de execução do swagger com Specification Arg Resolver e resolvendo problema do Specification Arg Resolver quando utilizamos o DevTools

--- templates/skeletons/java/simple/src/main/java/com/${{ values.orgName }}/${{ values.name }}/${{ values.springApplicationName }}Application.java
+++ templates/skeletons/java/simple/src/main/java/com/${{ values.orgName }}/${{ values.name }}/${{ values.springApplicationName }}Application.java
@@ -2,8 +2,10 @@ package com.${{ values.orgName }}.${{ values.name }};
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ${{ values.springApplicationName }}Application {
 
 	public static void main(String[] args) {
